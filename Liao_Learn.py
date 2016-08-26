f49be8deffe5b06f81a50ad9bcedc6ef8d48182b
WangAngle <wang1091392040@163.com>
2016-08-26 20:59:00 +0800

    Creat Python file

---
+++ Liao_Learn.py
@@ -0,0 +1,25 @@
+#coding = utf-8
+
+#1 输入和输出
+#1.1
+print('hello ,world')
+#1.2
+print(300)
+print(100+200)
+print('100 + 200 =',100+200)
+
+
+#2.数据类型和变量
+
+
+
+
+#2 函数
+#2.1 调用函数
+print('绝对值:',abs(-100))
+
+#2.2 定义函数
+def Func_sum(a,b):
+	return (a + b)
+	
+print('Func_sum:',Func_sum(25,36))
